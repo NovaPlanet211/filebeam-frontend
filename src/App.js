d1acda5cd0ebfb637d9cf5abbc8d3f1338027da8
Damian <novaplanetbiznes@gmail.com>
2025-10-15 13:25:04 +0200

    nowa nazwa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { useState, useEffect } from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import LoadingScreen from "./LoadingScreen";
 import UploadForm from "./UploadForm";
 import AdminPanel from "./AdminPanel";
 
 function App() {
-  
+  const [loadingDone, setLoadingDone] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoadingDone(true);
+    }, 2000); // 2 sekundy animacji
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!loadingDone) {
+    return <LoadingScreen />;
+  }
+
   return (
     <Router>
       <Routes>
@@ -13,14 +27,8 @@ function App() {
       </Routes>
     </Router>
   );
-const [loadingDone, setLoadingDone] = useState(false);
- return (
-    <>
-      {!loadingDone && <LoadingScreen onFinish={() => setLoadingDone(true)} />}
-      {loadingDone && <MainApp />}
-    </>
-  ); 
 }
 
 export default App;
+
 
